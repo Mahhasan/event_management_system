9d4e0b630bf66815a786d8a1eb26083a7fbfb380
Md Jannat UL Naim <jannatulnayem446@gmail.com>
2023-04-05 00:32:18 +0600

    Updated

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,7 +7,7 @@
     @if(session()->has('success'))
     <div class="alert alert-success">{{ session('success') }}</div>
     @endif
-    <table class="table">
+    {{-- <table class="table">
         <thead>
             <tr>
             <th></th>
@@ -46,7 +46,47 @@
             </tr>
         @endforeach
         </tbody>
-    </table>
-    {{ $events->links() }}
+    </table> --}}
+
+    <div class="row">
+        @foreach($events as $event)
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <img src="img/{{ $event->image }}" class="card-img-top" alt="{{ $event->title }}">
+                    <div class="row">
+                        <div class="col-sm-7 text-center">
+                            <small style="font-size: 11px; color: #ffae00;" class=""><i class="fa fa-clock" aria-hidden="true"></i><b>{{ \Carbon\Carbon::parse($event->start_datetime)->format('F j, Y g:i A') }}</b></small>
+                        </div>
+                        <div class="col-sm-5 text-center">
+                            <small class="text-info"><i class="fa fa-map-marker" aria-hidden="true"></i> <b>{{ $event->location }}</b></small>
+                        </div>
+                    </div>
+                    {{-- <div class="text-right pr-2">
+                        
+                        <small><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $event->location }}</small>
+                    </div> --}}
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        {{-- <div class="row text-center">
+                            <div class="col-sm-6">
+                                <small style="font-size: 11px;" class=""><b><i class="fa fa-clock" aria-hidden="true"></i> Start Time</b><br> {{ \Carbon\Carbon::parse($event->start_datetime)->format('j F Y, g:i A') }}</small>
+                            </div>
+                            <div class="col-sm-6">
+                                <small style="font-size: 11px;" class=""><b><i class="fa fa-clock-" aria-hidden="true"></i> End Time</b><br> {{ \Carbon\Carbon::parse($event->end_datetime)->format('j F Y, g:i A') }} </small>
+                            </div>
+                        </div> --}}
+                        <div class="row text-center">
+                            <div class="col-sm-6">
+                                <a href="{{ route('registrations.create', $event) }}" class="btn btn-outline-primary btn-sm btn-block">Register</a>
+                            </div>
+                            <div class="col-sm-6">  
+                                <a href="{{ route('events.show', $event) }}" class="btn btn-outline-info btn-sm btn-block">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,7 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
+<small class="text-left ml-1"><a href="{{ url()->previous() }}"><i class="fa fa-arrow-left"></i> Get Back</a></small>
+<div class="card text-center">
+    <div class="card-header">
+      {{ $event->title }}
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-4">
+                <img id="blah" src="/img/{{ $event->image}}" width="240" height="240" class="rounded" alt="your image" />
+                <div class="mt-3">
+                    <strong><i class="fa fa-map-marker" aria-hidden="true"></i> Event Location:</strong> {{ $event->location }}
+                </div>
+                <a href="{{ route('registrations.create', $event) }}" class="btn btn-outline-primary btn-sm btn-block">Get Register</a>
+            </div>
+            <div class="col-md-8 text-left">
+                <div class="mb-3">
+                    <strong>Description:</strong> {{ $event->description }}
+                </div>
+                <div class="mb-3">
+                    <strong>Start Date:</strong> {{ \Carbon\Carbon::parse($event->start_datetime)->format('F j, Y g:i A') }}
+                </div>
+                <div class="mb-3">
+                    <strong>End Date:</strong> {{ \Carbon\Carbon::parse($event->end_datetime)->format('F j, Y g:i A') }}
+                </div>
+               
+                <div class="mb-3">
+                    <strong>Price:</strong> {{ $event->price }}
+                </div>
+                <div class="mb-3">
+                    <strong>Capacity:</strong> {{ $event->capacity }}
+                </div>
+                <a href="{{ route('events.edit', $event) }}" class="btn btn-primary">Edit</a>
+                <form action="{{ route('events.destroy', $event) }}" method="POST" style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>  
+            </div>
+        </div>
+    </div>
+  </div>
+{{-- <div class="container">
     <h1>{{ $event->title }}</h1>
     <div class="mb-3">
         <strong>Image:</strong><img id="blah" src="/img/{{ $event->image}}" width="240" height="240" class="rounded-circle" alt="your image" /> <br>
@@ -30,7 +71,7 @@
         @method('DELETE')
         <button type="submit" class="btn btn-danger">Delete</button>
     </form>
-</div>
+</div> --}}
 <br><br>
 <div class="container">
         <div class="row justify-content-center">
